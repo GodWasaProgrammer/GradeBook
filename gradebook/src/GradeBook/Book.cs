8f9fa3c0878c5b1dbd6a59c4dd1e13ed8caf6af1
bwnils@gmail.com <bwnils@gmail.com>
2022-01-24 16:31:58 +0100

    Weekend Update

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -7,7 +7,7 @@ namespace GradeBook
         public Book(string name)
         {
             grades = new List<double>();
-            this.name = name;
+            Name = name;
         }
 
         public void AddGrade(double grade)
@@ -16,7 +16,7 @@ namespace GradeBook
         }
 
         List<Double> grades;
-        private string name;
+        public string Name;
 
         public Statistics GetStatistics()
         
